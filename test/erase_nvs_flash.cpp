99faad224f45619c694093cfa538f0fa72ff57d9
samuko-things <samuel.c.agba@gmail.com>
2025-09-04 07:13:16 +0100

    cleanup

--- test/erase_nvs_flash.cpp
+++ test/erase_nvs_flash.cpp
@@ -1,8 +1,13 @@
 #include <nvs_flash.h>
+#include <Arduino.h>
 
 void setup() {
   nvs_flash_erase(); // erase the NVS partition and...
   nvs_flash_init(); // initialize the NVS partition.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
   while(true);
 }
 
